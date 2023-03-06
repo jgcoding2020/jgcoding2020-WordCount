625e77aaf14b9f43d3943d9239f5d6b238dea369
Joshua David Gardner <jgardn11@yahoo.com>
2023-03-06 23:25:16 +0000

    Complete

--- src/main/java/ReturnWordCount.java
+++ src/main/java/ReturnWordCount.java
@@ -5,6 +5,8 @@ public class ReturnWordCount {
      * @return return the amount of words in a string.
      */
     public int count(String in){
-        return 0;
+        String[] wordArray = in.split(" ");
+        int count = wordArray.length;
+        return count;
     }
 }
